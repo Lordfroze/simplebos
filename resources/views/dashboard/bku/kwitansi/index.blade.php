bce84e5120df6facfbbb5abd3cb159424f7c7602
lordfroze <yogaadipratama22@gmail.com>
2025-07-16 18:49:48 +0700

    fix header

--- resources/views/dashboard/bku/kwitansi/index.blade.php
+++ resources/views/dashboard/bku/kwitansi/index.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 @section('title')
-    Cetak Kwitansi
+Cetak Kwitansi
 @endsection
 
 @section('content')
-@endsection
+<!-- Main content -->
+@if (session('success'))
+<div class="alert alert-success">
+  {{ session('success') }}
+</div>
+@endif
 
+<div class="content">
+<p>Silahkan pilih template kwitansi yang akan di print</p>
+</div>
+
+@endsection
